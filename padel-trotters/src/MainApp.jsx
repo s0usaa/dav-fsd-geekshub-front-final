edf2def17ed01105c85087c1c9f9763ad528d3cb
David Soto Sousa <sousavlc86@gmail.com>
2023-04-27 18:17:46 +0200

    feat: Estructura basica de react, MainApp e instalacion de dependencias

---
+++ padel-trotters/src/MainApp.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './MainApp.css';
+
+export const MainApp = () => {
+  return (
+    <div>MainApp</div>
+  )
+}
--- padel-trotters/src/main.jsx
+++ padel-trotters/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowseRouter} from 'react-router-dom';
+import { MainApp } from './MainApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <MainApp />
   </React.StrictMode>,
 )
