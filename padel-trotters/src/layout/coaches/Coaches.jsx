d98d512d3c40355653e34cd3331ae59f09dabcff
David Soto Sousa <sousavlc86@gmail.com>
2023-05-11 21:17:12 +0200

    feat: Iniciando estilos de la pagina que muestra los entrenadores

--- padel-trotters/src/layout/coaches/Coaches.jsx
+++ padel-trotters/src/layout/coaches/Coaches.jsx
@@ -3,6 +3,11 @@ import { useSelector } from 'react-redux'
 import { userData } from '../../services/userSlice'
 import { viewCoaches } from '../../services/apiCalls';
 import Spinner from 'react-bootstrap/esm/Spinner';
+import Container from 'react-bootstrap/esm/Container';
+import Row from 'react-bootstrap/esm/Row';
+import Col from 'react-bootstrap/esm/Col';
+import Card from 'react-bootstrap/Card';
+import IMGCoach from '../../img/gente-jugando-al-padel-dentro.jpg';
 
 export const Coaches = () => {
   const credentialsRdx = useSelector(userData);
@@ -19,22 +24,51 @@ export const Coaches = () => {
     }
   },[coaches]);
   return (
-    <div>
-      {coaches.length > 0 ? (
-        <div>
-          {coaches.map((entrenadores)=>{
-            return (
-              <>
-              <div key={entrenadores.id}>
-              {entrenadores.especialidad}
-              </div>
-              </>
-            );
-          })}
-        </div>
-      ) : (
-        <Spinner animation="border" variant="info" />
-      )}
-    </div>
+    <Container fluid>
+      <Row className='d-flex justify-content-center tracksDesign'>
+        {coaches.length > 0 && credentialsRdx.credentials.usuario.roleId === 1 ? (
+          <Col>
+            {coaches.map((entrenadores)=>{
+              return(
+                <div key={entrenadores.id}>
+                <Card className='bg-dark text-white m-5'>
+                  <Card.Img src={IMGCoach} alt="Entrenadores" />
+                  <Card.ImgOverlay>
+        <Card.Title>Entrenador de {entrenadores.especialidad}</Card.Title>
+        <Card.Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit reiciendis recusandae esse quas eius quo perferendis eligendi, quam dolorem aspernatur.
+        </Card.Text>
+      </Card.ImgOverlay>
+                </Card>
+                </div>
+              )
+            })}
+          </Col>
+        ) : coaches.length > 0 && credentialsRdx.credentials.usuario.roleId === 2 ? (
+          <Col>
+            {coaches.map((entrenadores)=>{
+              return(
+                <div key={entrenadores.id}>
+                <Card className='bg-dark text-white m-5'>
+                  <Card.Img src={IMGCoach} alt="Entrenadores" />
+                  <Card.ImgOverlay>
+        <Card.Title>Entrenador de {entrenadores.especialidad}</Card.Title>
+        <Card.Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit reiciendis recusandae esse quas eius quo perferendis eligendi, quam dolorem aspernatur.
+        </Card.Text>
+      </Card.ImgOverlay>
+                </Card>
+                </div>
+              )
+            })}
+          </Col>
+        ) : (
+          <div>
+       <Spinner animation="border" variant="info"/>
+       <span>Cargando...</span>
+       </div>
+        )}
+      </Row>
+    </Container>
   )
 }
